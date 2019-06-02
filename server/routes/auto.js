97ba46f1617e22ce382193e110812c51bea01ebc
Eduardo Valdez <iryx_clow@hotmail.com>
2019-06-02 12:05:34 -0500

    Peticion PUT (Actualizar) Auto
    Se creó la petición que actualiza los datos de un auto dado su id.

--- server/routes/auto.js
+++ server/routes/auto.js
@@ -74,24 +74,12 @@ app.post('/auto', verificarToken, (req, res) => {
     let { body } = req;
     body._id = undefined;
     body.usuario = req.usuario._id;
-    body.extranjero = body.extranjero === null ? undefined : body.extranjero;
-    body.totalDuenos = body.totalDuenos === null ? undefined : body.totalDuenos;
-    body.totalAccidentes = body.totalAccidentes === null ? undefined : body.totalAccidentes;
-    body.transmision = body.transmision === null ? undefined : body.transmision;
-    body.alarma = body.alarma === null ? undefined : body.alarma;
-    body.aireAcondicionado = body.aireAcondicionado === null ? undefined : body.aireAcondicionado;
-    body.vidriosElectricos = body.vidriosElectricos === null ? undefined : body.vidriosElectricos;
-    body.puertasElectricas = body.puertasElectricas === null ? undefined : body.puertasElectricas;
-    body.bolsasDeAirePiloto = body.bolsasDeAirePiloto === null ? undefined : body.bolsasDeAirePiloto;
-    body.bolsasDeAirePasajero = body.bolsasDeAirePasajero === null ? undefined : body.bolsasDeAirePasajero;
-    body.bolsasDeAireLaterales = body.bolsasDeAireLaterales === null ? undefined : body.bolsasDeAireLaterales;
-    body.seguroDeNinos = body.seguroDeNinos === null ? undefined : body.seguroDeNinos;
-    body.controlDeEstabilidad = body.controlDeEstabilidad === null ? undefined : body.controlDeEstabilidad;
-    body.bluetooth = body.bluetooth === null ? undefined : body.bluetooth;
-    body.sensorFrontal = body.sensorFrontal === null ? undefined : body.sensorFrontal;
-    body.sensorTrasero = body.sensorTrasero === null ? undefined : body.sensorTrasero;
-    body.camaraTrasera = body.camaraTrasera === null ? undefined : body.camaraTrasera;
     body.imagenes = ['default.jpg'];
+    for (const propiedad in body) {
+        if (body[propiedad] === null) {
+            body[propiedad] = undefined;
+        }
+    }
     const auto = new Auto(body);
     auto.save((err, autoDB) => {
         if (err) {
@@ -101,6 +89,30 @@ app.post('/auto', verificarToken, (req, res) => {
             });
         }
         res.status(201).json({
+            ok: true,
+            auto: autoDB
+        });
+    });
+});
+
+app.put('/auto/:id', [verificarToken, verificarAuto], (req, res) => {
+    let { auto, body } = req;
+    body._id = undefined;
+    body.imagenes = undefined;
+    body.usuario = undefined;
+    for (const propiedad in auto) {
+        if (body[propiedad] !== null && body[propiedad] !== undefined) {
+            auto[propiedad] = body[propiedad];
+        }
+    }
+    auto.save((err, autoDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+        res.json({
             ok: true,
             auto: autoDB
         });
